5145c14845e9c059f9eda7f0b426b024072dfd92
Casey Zimmer <czimmer@ramapo.edu>
2018-08-20 23:07:25 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,5 +4,5 @@ def display_rainbow(colors)
   
   letters = ["R","O","Y","G","B","I","V"]
   
-  puts "#{letters[0]}, #{colors[0]}"
+  puts "#{letters[0]}, #{colors[0]} ,#{letters[1]}, #{colors[1]}, #{letters[2]}, #{colors[2]}"
 end
